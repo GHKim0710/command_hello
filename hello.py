51ec5af8737db0141fbb621b88ae717f5e721029
ghkim <kimgunhak7@naver.com>
2024-04-02 17:51:32 +0900

    conflict resolved GitHub

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 // For command line git tutorial
-// Local repository modification
+// First : Git modification
+// Second : Local repository modification
 
 print("Hello World")
 print("Tell your world")
